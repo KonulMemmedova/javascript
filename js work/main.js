7a6b1c4b967f5f4babcc6cf52e9d86c1d538e092
Könül_Heart <100348091+KonulMemmedova@users.noreply.github.com>
2022-05-11 17:17:55 +0400

    js_number

---
+++ js work/main.js
@@ -0,0 +1,16 @@
+// artirma ve azaltma isleri:
+let counter = 320
+counter = counter + 1 // uzun yolu
+counter += 1;
+counter ++;
+console.log(counter)
+
+counter --;
+counter -= 1;
+console.log(counter)
+
+counter *= 10;
+console.log(counter)
+
+counter /= 2;
+console.log(counter)
